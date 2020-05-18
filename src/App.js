134ee29f99c8b91cdff965eecacd5f4fbfaefb14
Kumar Prafull <eprafull@gmail.com>
2020-05-18 17:21:11 +0530

    congiguring react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import {navbar, navbarBrand} from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <navbar dark color="primary">
+        <div className="container">
+          <navbarBrand href="/">Ristorante Con Fusion</navbarBrand>
+        </div>
+      </navbar>
     </div>
   );
 }
